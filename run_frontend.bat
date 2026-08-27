ba285f96d6690bfaaead57c696ce2a6604d1caae
syban75 <syban75@gmail.com>
2026-08-27 21:21:35 +0900

    docker compose

--- run_frontend.bat
+++ run_frontend.bat
@@ -2,4 +2,4 @@
 
 cd /d "%~dp0src"
 
-"%~dp0.venv\Scripts\python.exe" -m streamlit run .\src\streamlit_app.py --server.port 8514 --server.address 0.0.0.0
+"%~dp0.venv\Scripts\python.exe" -m streamlit run streamlit_app.py --server.port 8514 --server.address 0.0.0.0
